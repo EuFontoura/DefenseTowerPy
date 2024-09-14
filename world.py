dcc959425cb1247b83ec5c36f988159ee6b59d97
Gabriel Fontoura <gabrielfmendonca@gmail.com>
2024-09-14 14:41:49 -0300

    adicionado spawn de torres

--- world.py
+++ world.py
@@ -1,11 +1,16 @@
 import pygame as pg
+import random
+from enemy_data import ENEMY_SPAWN_DATA
 
 class World():
     def __init__(self, data, map_image):
+        self.level = 1
         self.tile_map = []
         self.waypoints = []
         self.level_data = data
         self.image = map_image
+        self.enemy_list = []
+        self.spawned_enemies = 0
     
     def process_data(self):
         # look through data to extract relevant info
@@ -23,6 +28,15 @@ class World():
             temp_x = point.get("x")
             temp_y = point.get("y")
             self.waypoints.append((temp_x, temp_y))
+            
+    def process_enemies(self):
+        enemies = ENEMY_SPAWN_DATA[self.level - 1]
+        for enemy_type in enemies:
+            enemies_to_spawn = enemies[enemy_type]
+            for enemy in range(enemies_to_spawn):
+                self.enemy_list.append(enemy_type)
+        # now randomize the list to shuffle the enemies
+        random.shuffle(self.enemy_list)
 
     def draw(self, surface):
         surface.blit(self.image, (0, 0))
